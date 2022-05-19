d65b968968dc465c0d5190576e4bb4c3fa9f88b9
Rodrigo Medeiros <medeiros11.dev@gmail.com>
2022-05-19 10:39:57 -0300

    Código finalizado

--- day1.js
+++ day1.js
@@ -1,5 +1,5 @@
 let numeroUm = 1
-let stringUm = '4'
+let stringUm = '1'
 let numeroTrinta = 30
 let stringTrinta = '30'
 let numeroDez = 10
@@ -17,7 +17,7 @@ if (numeroTrinta === stringTrinta) {
   console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
 }
 
-if (numeroDez !== stringDez) {
+if (numeroDez == stringDez) {
   console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
 } else {
   console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
