252db4ece1dd882bbbf71ab597c2ea20fd9dd5d9
Rodrigo Medeiros <medeiros11.dev@gmail.com>
2022-05-25 14:34:50 -0300

    Código finalizado

--- day4.js
+++ day4.js
@@ -1,9 +1,3 @@
-/*Você deve criar um programinha que comece com um valor específico pré-definido entre 0 a 10 para o número que você vai adivinhar (7, por exemplo).
-
-Em seguida, o programa vai perguntar para você qual o valor que você deseja chutar e, caso você acerte, ele irá te parabenizar. Caso erre, ele vai te dar mais 2 tentativas.
-
-No fim, caso você não acerte nenhuma vez, ele vai imprimir qual era o número inicial.*/
-
 const valorInicial = Math.floor(Math.random() * (10 - 0 + 1) + 0);
 let acertou = false;
 for (let i=0; i<3; i++){
