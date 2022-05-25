f5cc25835bc6ada2628442612f2a9bba4028d126
Rodrigo Medeiros <medeiros11.dev@gmail.com>
2022-05-25 10:38:43 -0300

    Programa finalizado com número randomico

--- day4.js
+++ day4.js
@@ -4,3 +4,17 @@ Em seguida, o programa vai perguntar para você qual o valor que você deseja ch
 
 No fim, caso você não acerte nenhuma vez, ele vai imprimir qual era o número inicial.*/
 
+const valorInicial = Math.floor(Math.random() * (10 - 0 + 1) + 0);
+let acertou = false;
+for (let i=0; i<3; i++){
+    let valor1= prompt("Adivinhe qual número estou pensando de 0 a 10");
+    if (valorInicial == valor1) {
+        alert("Muito bem, você acertou !")
+        acertou = true;
+        break;
+    }
+    alert("Errado !")
+}
+    if(!acertou){
+        alert(`Infelizmente, você não acertou. O número era ${valorInicial}!`);
+    }
